95d505e37eb974e461d6d019534cdcfd61e9ca66
unknown <a.desantis@contrader.it>
2019-07-18 12:55:52 +0200

    new commit

--- db/sampledb.sql
+++ db/sampledb.sql
@@ -1,6 +1,8 @@
+CREATE DATABASE  IF NOT EXISTS `sampledb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `sampledb`;
 -- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: promo
+-- Host: localhost    Database: sampledb
 -- ------------------------------------------------------
 -- Server version	8.0.15
 
@@ -16,55 +18,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `admin`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `admin`;
+DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `admin` (
-  `idadmin` int(11) NOT NULL AUTO_INCREMENT,
-  `ausername` varchar(16) NOT NULL,
-  `apassword` varchar(32) NOT NULL,
-  `ausertype` varchar(45) DEFAULT NULL,
-  `barcode` int(4) NOT NULL,
-  PRIMARY KEY (`idadmin`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `admin`
---
-
-LOCK TABLES `admin` WRITE;
-/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
-INSERT INTO `admin` VALUES (1,'admin','admin','admin',0);
-/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `client`
---
-
-DROP TABLE IF EXISTS `client`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `client` (
-  `idclient` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
   `username` varchar(16) NOT NULL,
-  `cf` varchar(45) DEFAULT NULL,
+  `usertype` varchar(255) DEFAULT NULL,
   `password` varchar(32) NOT NULL,
-  PRIMARY KEY (`idclient`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `client`
+-- Dumping data for table `user`
 --
 
-LOCK TABLES `client` WRITE;
-/*!40000 ALTER TABLE `client` DISABLE KEYS */;
-/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('admin','ADMIN','admin',1),('user','USER','user',2);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -76,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-07-16 14:09:28
+-- Dump completed on 2019-06-12 13:10:18
