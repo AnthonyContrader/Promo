e730d6250b0a6f7a81fa834edf5c1bff1d9a84a2
simonegiorgio84 <calcifer.anacleto@gmail.com>
2019-07-19 17:56:37 +0200

    create HomeModerator

---
+++ src/main/java/it/contrader/view/HomeModeratorView.java
@@ -0,0 +1,60 @@
+package it.contrader.view;
+
+import it.contrader.controller.Request;
+import it.contrader.main.MainDispatcher;
+
+public class HomeModeratorView extends AbstractView {
+	
+
+    private String choice;
+    
+	private Request request;
+
+	/**
+	 * Se la request non è nulla mostra un messaggio di benvenuto
+	 */
+    public void showResults(Request request) {
+    	if(request!=null) {
+    	System.out.println("\n Benvenuto in PROMO "+request.get("username").toString() + "\n");
+    	}
+    }
+
+
+    /**
+     * Chiede all'utente di effettuare una scelta (da console)
+     */
+    public void showOptions() {
+        System.out.println("-------------MENU------------\n");
+        System.out.println(" Seleziona cosa vuoi gestire:");
+        System.out.println("[C]lienti [E]sci");
+        //Il metodo che salva l'input nella stringa choice.
+        //getInput() è definito in AbstractView.
+        choice = this.getInput();
+    }
+
+    /**
+     * Impacchetta una request (in base alla scelta sarà diversa) che invia ai controller tramite il
+     * Dispatcher
+     */
+    public void submit() {    
+    	//crea una nuova Request (vedi classe Request)
+    	request = new Request();
+        switch (choice) {
+        case "u":
+        	this.request.put("mode", "CLIENTLIST");
+        	MainDispatcher.getInstance().callAction("Client", "doControl", request);
+        	break;
+ 
+        case "e":
+        	MainDispatcher.getInstance().callAction("Login", "doControl", null);
+        	break;
+        default:
+        	
+            request.put("choice", choice);
+        	MainDispatcher.getInstance().callAction("Login", "doControl", request);
+        }
+    }
+
+	
+	
+}
