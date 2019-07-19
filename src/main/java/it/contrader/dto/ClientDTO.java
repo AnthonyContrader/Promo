fe2efd722760989e2d3806791e7fc8c8f7c6fb86
AtiruNest <delben.gerri@gmail.com>
2019-07-19 16:48:05 +0200

    progetto rifatto

---
+++ src/main/java/it/contrader/dto/ClientDTO.java
@@ -0,0 +1,79 @@
+package it.contrader.dto;
+
+public class ClientDTO {
+	private int id;
+	private int idAdmin;
+	private String username;
+	private String password;		
+	private String email;
+	private String scode;
+	
+	public  ClientDTO(String username, String password, String email, String scode) {
+		this.username=username;
+		this.password=password;
+		this.email=email;
+		this.scode=scode;
+	}
+	
+	public  ClientDTO(int idAdmin, String username, String password, String email, String scode) {
+		this.id=idAdmin;
+		this.username=username;
+		this.password=password;
+		this.email=email;
+		this.scode=scode;
+	}
+	
+	public  ClientDTO(int id, int idAdmin, String username, String password, String email, String scode) {
+		this.id=id;
+		this.idAdmin=idAdmin;
+		this.username=username;
+		this.password=password;
+		this.email=email;
+		this.scode=scode;
+	}
+	
+	public String getScode() {
+		return scode;
+	}
+
+	public void setScode(String scode) {
+		this.scode = scode;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getIdAdmin() {
+		return idAdmin;
+	}
+	public void setIdAdmin(int idAdmin) {
+		this.idAdmin = idAdmin;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return "Client [idClient=" + idClient + ", idAdmin=" + idAdmin + ", username=" + username + ", password="
+				+ password + ", email=" + email + "]";
+	}
+}
