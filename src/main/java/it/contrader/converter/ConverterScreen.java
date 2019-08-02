b2a2c00e31c12643ff7701054ac265b6b53ad21a
andreapirro90 <a.pirro@contrader.it>
2019-08-02 17:04:28 +0200

    screen update 2

---
+++ src/main/java/it/contrader/converter/ConverterScreen.java
@@ -0,0 +1,52 @@
+package it.contrader.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.contrader.dto.ScreenDTO;
+import it.contrader.model.Screen;
+
+public class ConverterScreen {
+
+	public static ScreenDTO toDTO(Screen screen) {
+		ScreenDTO screenDTO = null;
+		if (screen != null) {
+			screenDTO = new ScreenDTO();
+			screenDTO.setIdscreen(screen.getIdscreen());
+			screenDTO.setOutput(screen.getOutput());
+			screenDTO.setPosition(screen.getPosition());
+		}
+		return screenDTO;
+	}
+
+	public static Screen toEntity(ScreenDTO screenDTO) {
+		Screen screen = null;
+		if (screenDTO != null) {
+			screen = new Screen();
+			screen.setIdscreen(screenDTO.getIdscreen());
+			screen.setOutput(screenDTO.getOutput());
+			screen.setPosition(screenDTO.getPosition());
+		}
+		return screen;
+	}
+
+	public static List<ScreenDTO> toListDTO(List<Screen> list) {
+		List<ScreenDTO> listScreenDTO = new ArrayList<>();
+		if (!list.isEmpty()) {
+			for (Screen screen : list) {
+				listScreenDTO.add(ConverterScreen.toDTO(screen));
+			}
+		}
+		return listScreenDTO;
+	}
+
+	public static List<Screen> toListEntity(List<ScreenDTO> listScreenDTO) {
+		List<Screen> list = new ArrayList<>();
+		if (!listScreenDTO.isEmpty()) {
+			for (ScreenDTO screenDTO : listScreenDTO) {
+				list.add(ConverterScreen.toEntity(screenDTO));
+			}
+		}
+		return list;
+	}
+}
---
+++ src/main/java/it/contrader/services/ScreenService.java
@@ -0,0 +1,57 @@
+package it.contrader.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.contrader.converter.ConverterScreen;
+import it.contrader.dao.ScreenRepository;
+import it.contrader.dto.ScreenDTO;
+import it.contrader.model.Screen;
+
+@Service
+public class ScreenService {
+
+	private final ScreenRepository screenRepository;
+
+	@Autowired
+	public ScreenService(ScreenRepository screenRepository) {
+		this.screenRepository = screenRepository;
+	}
+
+	public List<ScreenDTO> getListaScreenDTO() {
+		return ConverterScreen.toListDTO((List<Screen>) screenRepository.findAll());
+	}
+
+	public ScreenDTO getScreenDTOById(Integer idscreen) {
+		return ConverterScreen.toDTO(screenRepository.findById(idscreen).get());
+	}
+
+	public boolean insertScreen(ScreenDTO screenDTO) {
+		return screenRepository.save(ConverterScreen.toEntity(screenDTO)) != null;
+	}
+	
+	public boolean insertNewScreen(ScreenDTO screenDTO) {
+		return screenRepository.save(ConverterScreen.toEntity(screenDTO)) != null;
+	}
+
+	public boolean updateScreen(ScreenDTO screenDTO) {
+		return screenRepository.save(ConverterScreen.toEntity(screenDTO)) != null;
+	}
+	
+	public void deleteScreenById(Integer idscreen) {
+		screenRepository.deleteById(idscreen);
+	}
+	
+	public List<ScreenDTO> findScreenDTOByOutput(String output) {
+		
+		final List<Screen> list = screenRepository.findAllByOutput(output);
+		final List<ScreenDTO> screenDTOs = new ArrayList<>();
+		list.forEach(i -> screenDTOs.add(ConverterScreen.toDTO(i)));
+		return screenDTOs;
+		
+	
+	}
+}
