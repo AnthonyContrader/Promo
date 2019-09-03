bde50cbc6420dce4661149f40ce2711dbe9f2c24
simonegiorgio84 <calcifer.anacleto@gmail.com>
2019-09-03 15:22:37 +0200

    crud client

--- src/service/client.service.ts
+++ src/service/client.service.ts
@@ -5,6 +5,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { LoginDTO } from 'src/dto/logindto';
 import { AreariservataDTO } from 'src/dto/areariservatadto';
+import { UserDTO } from 'src/dto/userdto';
 
 /**
  * I service sono decorati da @Injectable. 
@@ -21,8 +22,31 @@ export class ClientService extends AbstractService<ClientDTO>{
    
     constructor(http: HttpClient) {
         super(http);
-        this.type = 'client';
+        this.nome = 'micro1';
+        this.type = 'clients';
+        this.port = '8080';
     }
+    auth() {
+      const user = JSON.parse(localStorage.getItem('currentUser')) as UserDTO;
+  
+      console.log(user.authorities + 'user');
+      if (user) {
+        return 'Bearer ' + user.authorities;
+      } else {
+        return '';
+      }
+    }
+
+    userLogged(username: string) {
+      // console.log('qua: ', this.auth());
+       console.log(this.auth());
+       return this.http.get('http://localhost:8080/api/users/' + username, {
+         headers: {
+           Authorization: this.auth()
+         }
+       });
+      }
+
 
     login(loginDTO: LoginDTO): Observable<ClientDTO> {
         return this.http.post<any>('http://localhost:8080/' + this.type + '/login', loginDTO)
