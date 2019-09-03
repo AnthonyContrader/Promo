afa82ca440fe0301b30c462a017fcc6268555e8d
simonegiorgio84 <calcifer.anacleto@gmail.com>
2019-09-03 15:35:38 +0200

    crud screen

--- src/service/screen.service.ts
+++ src/service/screen.service.ts
@@ -3,6 +3,7 @@ import { AbstractService } from './abstractservice';
 import { ScreenDTO } from 'src/dto/screendto';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { UserDTO } from 'src/dto/userdto';
 
 /**
  * I service sono decorati da @Injectable. 
@@ -19,7 +20,30 @@ export class ScreenService extends AbstractService<ScreenDTO>{
    
     constructor(http: HttpClient) {
         super(http);
-        this.type = 'screen';
+        this.nome = 'micro1';
+        this.type = 'screens';
+        this.port = '8080';
     }
+    auth() {
+      const user = JSON.parse(localStorage.getItem('currentUser')) as UserDTO;
+  
+      console.log(user.authorities + 'user');
+      if (user) {
+        return 'Bearer ' + user.authorities;
+      } else {
+        return '';
+      }
+    }
+
+    userLogged(username: string) {
+      // console.log('qua: ', this.auth());
+       console.log(this.auth());
+       return this.http.get('http://localhost:8080/api/users/' + username, {
+         headers: {
+           Authorization: this.auth()
+         }
+       });
+      }
+
 
 }
